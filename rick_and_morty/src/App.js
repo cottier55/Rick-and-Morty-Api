3979a055fc9f1ef69b5829f8a973f5a3e1ac5ad4
cottier55 <cottiersolchu55@gmail.com>
2023-06-09 09:27:41 -0300

    Termine todos los puntos que se me pidio

--- rick_and_morty/src/App.js
+++ rick_and_morty/src/App.js
@@ -1,31 +1,108 @@
-import './App.css';
-//import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters from './data.js';
+import {useState, useEffect} from "react";
+import axios from "axios";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {removeFavorite} from "./redux/actions";
+import Detail from "./views/detail/detail";
+import About from "./views/about/about";
+import Cards from "./components/cards/Cards";
+import logoRM from "./assets/logorm-removebg-preview.png";
+import Navbar from "./components/navbar/Navbar";
+
+import ErrorPage from "./views/error/errorPage";
+import LandingPage from "./views/landingPage/landingPage";
+import Favorties from "./views/favorites/favorties";
+
+import "./App.css";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [access, setAccess] = useState(true);
 
-function onSearch(characterID){
-   window.alert(characterID)
-}
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-   return (
-      <div className='App'>
-         <SearchBar onSearch={onSearch}/>
-         <Cards characters={characters}/>
-         {/* <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-   /> */}
-      </div>
-   );
+  const EMAIL = "cottiersolchu55@gmail.com";
+  const PASSWORD = "15S15o15l";
+
+  function login(userData) {
+    if (userData.password === PASSWORD && userData.email === EMAIL) {
+      setAccess(true);
+      navigate("/home");
+    }
+  }
+
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access]);
+
+  function searchHandler(id) {
+    // setCharacters([...characters, example]);
+
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(({data}) => {
+      if (data.name) {
+        setCharacters((oldChars) => [...oldChars, data]);
+      } else {
+        window.alert("¡No hay personajes con este ID!");
+      }
+    });
+  }
+
+  function closeHandler(id) {
+    let deleted = characters.filter((character) => character.id !== Number(id));
+
+    dispatch(removeFavorite(id));
+
+    setCharacters(deleted);
+  }
+
+  function randomHandler() {
+    let haveIt = [];
+    //Generate random number
+    let random = (Math.random() * 826).toFixed();
+
+    //Coerce to number by boxing
+    random = Number(random);
+
+    if (!haveIt.includes(random)) {
+      haveIt.push(random);
+      fetch(`https://rickandmortyapi.com/api/character/${random}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+          } else {
+            window.alert("No hay personajes con ese ID");
+          }
+        });
+    } else {
+      console.log("Ya agregaste todos los personajes");
+      return false;
+    }
+  }
+
+  return (
+    <div className="App">
+      <img className="title" src={logoRM} alt="logo" />
+
+      {location.pathname !== "/" && (
+        <Navbar onSearch={searchHandler} random={randomHandler} />
+      )}
+
+      <Routes>
+        <Route path="/" element={<LandingPage login={login} />} />
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={closeHandler} />}
+        />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/favorites" element={<Favorties />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ rick_and_morty/src/views/about/about.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1>Mi nombre es Solange y cree mi primera Api de Rick and Morty!!!</h1>
+    </div>
+  );
+}
+
+export default About;
